a331d4fad61b6ebe6084cd42ce540aee2d731607
eliteabhi <abhirangarajan2003@gmail.com>
2023-06-12 18:48:04 -0500

    Updated runner to check if pigpiod is enabled

--- runner.sh
+++ runner.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
+echo "Checking if pigpiod is active..."
+pigpiod_status=$(systemctl is-active pigpiod)
+echo "Pigpiod status: $pigpiod_status"
+if [[ $pigpiod_status != 'active' ]]
+then
+
+echo "Starting pigpiod..."
+sudo systemctl start pigpiod
+echo "...Started pigpiod"
+
+fi
+echo "You're good to go!"
+
+sleep 1s
+
 hd=$PWD
 cd ~/PICode
 
-if [ -e ~/PICode/ABORTED ] 
+if [ -e ./ABORTED ] 
 then
-    rm ~/PICode/ABORTED
+    rm ./ABORTED
 fi
 
 if ! screen -list | grep -q "picode"
